3a43b10a3dbcdaa7419921d75e000cab02b03624
Joel Burdorf <joelburdorf@Joels-MBP.netgear.com>
2019-12-27 09:25:47 -0600

    Completed another challenge

--- client.js
+++ client.js
@@ -1,16 +1,29 @@
 let allTheWords = [ 'calculating', 'guitar', 'various', 'thirsty', 'history', 'friend', 'discreet', 'godly', 'grip', 'wait', 'humor', 'agonizing', 'jazzy', 'sail', 'love', 'crow', 'giants', 'ball', 'curl', 'vivacious', 'excuse', 'extend', 'black', 'rabid', 'shirt', 'prefer', 'cup', 'mundane', 'nimble', 'unbiased', 'meal', 'overflow', 'rhetorical', 'clam', 'dusty', 'dirty', 'understood', 'royal', 'cloistered', 'dry', 'redundant', 'letter', 'wool', 'scattered', 'ear', 'offer', 'auspicious', 'funny', 'nostalgic', 'crash', 'crib', 'error', 'five', 'step', 'powder', 'minute', 'exotic', 'pedal', 'mountain', 'appliance', 'girls', 'pour', 'closed', 'unbecoming', 'button', 'glistening', 'press', 'previous', 'shy', 'fool', 'peck', 'warlike', 'end', 'sugar', 'jittery', 'wealth', 'applaud', 'flight', 'tiny', 'drop', 'bubble', 'cluttered', 'famous', 'sisters', 'fly', 'lick', 'brake', 'lazy', 'debt', 'breathe', 'makeshift', 'day', 'book', 'curve', 'mend', 'fortunate', 'breezy', 'tremble', 'hall', 'house' ];
 
 // console.log how many words are in the array
-console.log(allTheWords.length)
+console.log(allTheWords.length);
 
 // console.log "true" if allTheWords contains the word "error", "false" if it does not
-for(let i =0; i<allTheWords.length; i++){
-  if( i='error'){
-  console.log(true);
+for (let i=0; i<allTheWords.length; i++){
+  if ( allTheWords[i] === 'error'){
+    console.log(true);
 } else {
   console.log(false);
 }
 }
+
+//console.log(allTheWords.indexOf('error'));  //I discoverded that my 'else' statement did not log 'false' when I changed 'error' in the array to 'erro'.
+                                            // so I used indexOf to see if 'error' was still in the array, but returned -1. I then knew that even after removing
+                                            // 'error', the if statement still logged 'true'. So there was somthing wrong with my 'if' statement
+                                            // Solution was to make 'if' statement conditional an absolute === instead of only =
+
+//for (errorWord of allTheWords){
+//  if (errorWord === 'error'){
+//    console.log(' is true')
+//  } else {
+//    console.log('is false')
+//  }
+//}
 
 // console.log an array of all words that are longer than 7 characters
 const sevenPlus = allTheWords.filter(word => {    //.filter returns an array of elements after filtering out certain elements. This uses a callback function
@@ -20,7 +33,16 @@ console.log(sevenPlus);
 
 // console.log an array of all words that do not contain the letter "e"
 
+const matchWithOutE = allTheWords.filter(s => !s.includes('e'));
+
+console.log(matchWithOutE);
+
+
 // console.log an array of all words that either start with the letter "s" or end with "ing"
+
+// find all strings in array containing 'thi'
+
+
 
 // console.log the average word length in the array
 
